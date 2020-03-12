588ec9c00750555dffb30a6c39cdf36e8f4e2b02
seattleowl <seattleowl@users.noreply.github.com>
2020-03-12 16:00:57 -0700

    Add join functions

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const http = require("http").createServer(app)
 const app = express()
 const io = require("socket.io")(http)
 
+let races = {
+    2031: {
+        name: "TEST RACE",
+        txt: "Typing Racers",
+        racers: []
+    }
+}
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-io.on("connection", () => {
-    console.log("yeet!")
+io.on("connection", (socket) => {
+    socket.on("join", (data) => {
+        races[data.raceid].racers[data.username] = 0
+        io.emit("join")
+    })
 })
 
 http.listen(process.env.PORT, () => {
